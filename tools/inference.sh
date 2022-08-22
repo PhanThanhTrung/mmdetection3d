55473110967c42314e1e76180712e06d131f50b4
PhanThanhTrung <trung99nb@gmail.com>
2022-08-22 09:22:29 +0000

    inference script

---
+++ tools/inference.sh
@@ -0,0 +1,4 @@
+python3 tools/inference.py configs/fcos3d/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d.py\
+                           ./fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_20210715_235813-4bed5239.pth\
+                           --show-dir\
+                           ./data/scannet/show_results
